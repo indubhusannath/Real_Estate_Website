0ced74a617c747112ff33167b9493b5b39323aaa
Indu Bhusan Nath <ibnscope@gmail.com>
2022-12-02 20:16:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+     <head>
+        <title>Oyrawat Properties|Residential|Commercial|Affordable</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script><script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="./css/gen.css">
+    
+    </head>
+ <body>
+    <!--Header-->
+     <div class="container-fluid">
+         <div class="row" style="background-color:darkblue;">
+             <div class="col-sm-1">
+                <img src="./main_images/logo.jpeg" class="img-responsive img-thumbnail" alt="logo">
+             </div>
+             <div class="col-sm-11">
+                 <section class="align-middle" style="background-color:whitesmoke;border-bottom:5px solid darkorange;border-radius:0px 0px 0px 55px;">
+                  
+                
+                 <p class="font font_size_2 text-end" style="color:slategray;font-weight:400px;">&#9213; &nbsp;&nbsp;Commercial:&nbsp;&nbsp;(+91)1234567898 &#124;&nbsp;&nbsp;Residential:
+                 &nbsp;&nbsp;(+91)1234567898 &#124;&nbsp;&nbsp;Affordable:&nbsp;&nbsp;(+91)123456787
+                 &#124;&nbsp;&nbsp;Affordable:&nbsp;&nbsp;(+91)033-1234456
+                
+                 </p>
+                 </section>
+                 
+                 <section class="align-middle text-end font font_size_3 font-weight-bold" style="font-weight:450px;background-color:none;">
+                     
+                 <nav class="navbar navbar-expand-lg navbar-light bg-transperant">
+                     <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                       <ul class="navbar-nav">
+                         <li class="nav-item">
+                              <a class="nav-link" href="index.php" style="color:darkorange;">Home</a>
+                         </li>
+                         <li class="nav-item dropdown">
+                            <a style="color:powderblue;" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                              About Us
+                            </a>
+                                
+                             <div style="width:800px;" class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <section class="column">
+                                     <div style="float:right;width:70%;padding:10px;background-color:blue;border-radius:14px 14px 14px 14px;">
+                                       <p style="color:powderblue;">
+                                       Since we have entered into the property market.
+                                       We will focus on the personal and property values.
+                                     </p>
+                                     </div>
+
+                                 </section>
+                                 <section class="column">
+                                 <a class="dropdown-item" href="aboutus.php">&nbsp;&#62;&nbsp;Company Overview</a>
+                                 <a class="dropdown-item" href="services.php">&nbsp;&#62;&nbsp;Our Services</a>
+                                 <a class="dropdown-item" href="awards.php">&nbsp;&#62;&nbsp;Awards Gallery</a>
+                                 </section>
+
+                                 <section class="column">
+                                 <a class="dropdown-item" href="director.php">&nbsp;&#62;&nbsp;Director's Message</a>
+                                 <a class="dropdown-item" href="partner.php">&nbsp;&#62;&nbsp;Our Partners</a>
+                                 
+                                 </section>
+                            
+                                </div>
+                                
+                          </li>
+
+                     </ul>
+                     </div>
+                 </nav>
+
+                   
+
+                 </section>
+
+
+                    
+                 
+              
+             </div>
+         </div>
+
+
+     </div>
+
+     
+
+ </body>
+
+
+</html>
